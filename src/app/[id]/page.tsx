3e7d5a5596bdb110aaf36c3ea38fccd9c9d4b090
pierre goeddertz <pierre@MacBookPro.fritz.box>
2025-07-21 20:29:52 +0200

    Remove catch-all [id] route to fix 404 on main pages

--- src/app/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-import Home from '../page';
-
-interface Props { params: Promise<{ id: string }> }
-
-export default async function BackgroundPage({ params: _ }: Props) {
-  // Die Hauptseite im Hintergrund anzeigen, während das Modal darüber liegt
-  return <Home />;
-} 
